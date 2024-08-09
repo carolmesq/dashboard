41bdf6cd731f7c4e0de080e8690f97162191a993
carolmesq <carolmesq@gmail.com>
2024-08-09 20:49:08 -0300

    terc

--- dash.py
+++ dash.py
@@ -5,15 +5,38 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-df = pd.read_excel("C:/Users/Carol/Documents/GitHub/Carol/dados_gerais.xls")
+# Initialize connection to the PostgreSQL database
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(
+        host=st.secrets["host"],
+        port=st.secrets["port"],
+        database=st.secrets["database"],
+        user=st.secrets["user"],
+        password=st.secrets["password"]
+    )
 
-uf = st.sidebar.selectbox("UF", df["UF"].unique())
+conn = init_connection()
 
-df_filtered = df[df["UF"] == uf]
+# Function to run a SQL query with caching
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-municipio = st.sidebar.selectbox("Município", df_filtered["NOME_MUNIC"].unique())
+rows = run_query("SELECT uf, nome_munic, pop_2022 FROM t_analises.dados_gerais")
+df = pd.DataFrame(rows, columns=['uf', 'nome_munic','pop_2022'])
 
-df_fil_mun = df_filtered[df_filtered["NOME_MUNIC"] == municipio]
+#df = pd.read_excel("C:/Users/Carol/Documents/GitHub/Carol/dados_gerais.xls")
+
+uf = st.sidebar.selectbox("UF", df["uf"].unique())
+
+df_filtered = df[df["uf"] == uf]
+
+municipio = st.sidebar.selectbox("Município", df_filtered["nome_munic"].unique())
+
+df_fil_mun = df_filtered[df_filtered["nome_munic"] == municipio]
 
 col1, col2 = st.columns(2)
 
@@ -23,13 +46,13 @@ st.divider()
 col3, col4, col5 = st.columns(3)
 
 # Grafico linha população 2022
-fig_pop = px.bar(df_fil_mun, x="NOME_MUNIC", y="POP_2022", title = "ESTADOS")
+fig_pop = px.bar(df_fil_mun, x="nome_munic", y="pop_2022", title = "ESTADOS")
 col1.plotly_chart(fig_pop, use_container_width=True)
 
 
 
 # frota
-fig_frota = px.bar(df_fil_mun, x="VEIC_2010", y="AUTO_2010", title = "FROTA", orientation="h")
+fig_frota = px.bar(df_fil_mun, x="nome_munic", y="pop_2022", title = "FROTA", orientation="h")
 col2.plotly_chart(fig_frota, use_container_width=True)
 
 
